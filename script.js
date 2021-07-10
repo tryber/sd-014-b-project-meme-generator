628442b25e3a2e12b21b42fed4a3a1ecf1c7eaa7
Thyara <thyaranonato@outlook.com>
2021-07-10 17:48:04 -0300

    Adiciona requisitos #5 e #6

--- script.js
+++ script.js
@@ -7,7 +7,24 @@ const buttonFire = document.getElementById('fire');
 const buttonWater = document.getElementById('water');
 const buttonEarth = document.getElementById('earth');
 
+// Requisito #1
 function alteraTexto() {
   memeInput.innerText = textInput.value;
 }
-textInput.addEventListener('keyup',alteraTexto);
+textInput.addEventListener('keyup', alteraTexto);
+
+// Requisito #6
+function butFire() {
+  container.style.border = 'dashed 3px red';
+}
+buttonFire.addEventListener('click', butFire);
+
+function butWater() {
+  container.style.border = 'double 5px blue';
+}
+buttonWater.addEventListener('click', butWater);
+
+function butEarth() {
+  container.style.border = 'groove 6px green';
+}
+buttonEarth.addEventListener('click', butEarth);
