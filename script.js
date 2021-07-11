5160fe8b5d6f527d94adecc59b327ee5ced22025
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-07-11 17:51:19 -0300

    Atende requisito 6

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const h2 = document.getElementById('meme-text');
 const selectedImage = document.getElementById('meme-image');
 const chooseImage = document.getElementById('meme-insert');
 
-
 function showMemeText(){
 h2.innerText = input.value;
 }
@@ -22,6 +21,26 @@ console.log(selectedFile);
   
 }
     
-
-
 chooseImage.addEventListener('change', insertImage);
+
+
+function changeBorderContainer(event){
+    if(event.target.id === 'fire'){
+        memePlace.style.border = '3px dashed red';
+    }else if(event.target.id === 'water'){
+        memePlace.style.border = '5px double blue';
+    }else if(event.target.id === 'earth'){
+        memePlace.style.border = '6px groove green';
+    }
+}
+
+function addEventOnButtons(){
+    const btn = document.querySelectorAll("button");
+    for(let key of btn){
+        key.addEventListener('click', changeBorderContainer);
+    }
+
+}
+addEventOnButtons();
+
+
