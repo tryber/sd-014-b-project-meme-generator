7d30c504f60c0151a679e9b7f8e29147e76f4283
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-07-11 16:44:36 -0300

    Atende requisitos 2 e 3

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const input = document.getElementById('text-input');
 const memePlace = document.getElementById('meme-image-container');
 const h2 = document.getElementById('meme-text');
+const selectedImage = document.getElementById('meme-image');
+const chooseImage = document.getElementById('meme-insert');
+
 
 function showMemeText(){
 h2.innerText = input.value;
 }
 
 input.addEventListener('keyup', showMemeText);
+
+function insertImage(event){
+   const selectedFile = event.target.files[0];
+console.log(selectedFile);
+   const addressFile = new FileReader();
+   addressFile.onloadend = function(){
+       selectedImage.setAttribute('src', addressFile.result);
+   }
+   addressFile.readAsDataURL(selectedFile);
+  
+}
+    
+
+
+chooseImage.addEventListener('change', insertImage);
