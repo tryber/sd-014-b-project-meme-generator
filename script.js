196551bb19d6e606138dedc5f885e43b7bcdd8fc
brenoMatias <matiasbreno19@gmail.com>
2021-07-12 12:06:53 -0300

    Requisito 1 completo

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function addText(event) {
-    if (event.target.id === 'text-input') {
-      memeText.innerHTML = event.target.value;
-    } else {
-      memeBottomText.innerHTML = event.target.value;
-    }
-  }
+const textInput = document.querySelector('#text-input'); // acessa o valor do input
+const memeText = document.querySelector('#meme-text'); // acessa o container (div) onde o texto será(está) inserido
+function addText() {
+  memeText.innerText = textInput.value;
+}
+textInput.addEventListener('keyup', addText); // The keyup event is fired when a key is released.
+// este evento será disparado toda vez que uma tecla for digitada no teclado escrevendo o texto e o evento ocorre dentro do valor do input
