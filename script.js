37608446fc3115d9f084fc67198b9402a0e28a34
Lucas Alves <lucalves.rodrigues@gmail.com>
2021-07-12 18:11:07 -0300

    Requisito #2

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let input = document.getElementById('text-input');
 let textWrite = document.getElementById('meme-text');
+let image = document.getElementById('meme-insert');
+let imageInput = document.getElementById('meme-image');
+let botaoEnviar = document.getElementById('atualiza-imagem')
 
 
 /* Requisito #1 */
@@ -12,3 +15,13 @@ input.addEventListener('keyup', () => {
 
 
 /* Requisito #2 */
+image.addEventListener('change', recebeImagem);
+function recebeImagem () {
+    let leitorDaImagem = new FileReader()
+    let imagemRecebida = image.files[0]
+    leitorDaImagem.readAsDataURL(imagemRecebida);
+        leitorDaImagem.addEventListener('loadend', (load) => {
+            imageInput.src = load.target.result
+        })
+        
+}
