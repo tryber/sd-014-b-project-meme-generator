8c22559500d210871f6ae1f561d37bfcf91468c6
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-07-12 15:22:25 -0300

    Requisito 2 Completo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const textInput = document.getElementById('text-input');
-const memeImage = document.getElementById('meme-image');
+
 const memeText = document.getElementById('meme-text');
 
 function text() {
@@ -7,3 +7,17 @@ function text() {
 }
 
 textInput.addEventListener('keyup', text);
+
+const imgInput = document.getElementById('meme-insert');
+const memeImage = document.getElementById('meme-image');
+
+function image(imagem) {
+  const arquivo = imagem.target.files.item(0);
+  const endereco = new FileReader();
+  endereco.onloadend = function() {
+    memeImage.setAttribute('src', endereco.result);
+  };
+  endereco.readAsDataURL(arquivo);
+}
+
+imgInput.addEventListener("change", image);
