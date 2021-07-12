b02ab4fd61225d74c1ffea8ccf876ab89a210112
Thyara <thyaranonato@outlook.com>
2021-07-12 14:56:38 -0300

    Finaliza projeto

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ const memeImage = document.getElementById('meme-image');
 const buttonFire = document.getElementById('fire');
 const buttonWater = document.getElementById('water');
 const buttonEarth = document.getElementById('earth');
+const imagensExemplo = document.getElementById('exemplos-container');
 
 // Requisito #1
 function alteraTexto() {
-  memeInput.innerText = textInput.value;
+  memeText.innerText = textInput.value;
 }
 textInput.addEventListener('keyup', alteraTexto);
+
+// Requisito #2
+// Source: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded
+function escolherImg(event) {
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+}
+memeInput.addEventListener('change', escolherImg);
 
 // Requisito #6
 function butFire() {
@@ -28,3 +36,9 @@ function butEarth() {
   container.style.border = 'groove 6px green';
 }
 buttonEarth.addEventListener('click', butEarth);
+
+// Requisito #7
+function imgSelecionada(event) {
+  memeImage.src = event.target.src;
+}
+imagensExemplo.addEventListener('click', imgSelecionada);
