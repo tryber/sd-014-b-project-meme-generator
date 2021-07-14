9b9cbe8b9d446e6d0ffa84790d8624371db140dc
Riba Jr <ribastudio@gmail.com>
2021-07-13 23:49:34 -0300

    Ajustes iniciais do CSS, HTML e JavaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+const textInput = document.getElementById('text-input');
+const insertMeme = document.getElementById('meme-insert');
+const memeContainer = document.getElementById('meme-image-container');
+const memeImg = document.getElementById('meme-image');
+const memeTxt = document.getElementById('meme-text');
+const btnFire = document.getElementById('fire');
+const btnWater = document.getElementById('water');
+const btnEarth = document.getElementById('earth');
+
