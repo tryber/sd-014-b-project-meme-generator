cb538ceb3eebed71eee4ccf6dfda53742645de31
Riba Jr <ribastudio@gmail.com>
2021-07-14 00:27:04 -0300

    Arquivos de imagem adicionados, função inserir texto criada

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
-const textInput = document.getElementById('text-input');
-const insertMeme = document.getElementById('meme-insert');
+const textInput = document.querySelector('#text-input');
+const insertMeme = document.querySelector('#meme-insert');
 const memeContainer = document.getElementById('meme-image-container');
 const memeImg = document.getElementById('meme-image');
 const memeTxt = document.getElementById('meme-text');
 const btnFire = document.getElementById('fire');
 const btnWater = document.getElementById('water');
 const btnEarth = document.getElementById('earth');
+const btnInsertTxt = document.getElementById('button-insert');
 
+function verifyTextMeme() {
+  const inputValue = textInput.value;
+  if (inputValue === '') {
+    alert('Seu meme precisa ter um texto!');
+  }
+  
+}
+
+function addMemeText() {
+  const inputValue = textInput.value;
+  verifyTextMeme();
+  if (memeTxt.innerText === '') {
+    memeTxt.innerText = inputValue;
+    textInput.value = '';
+  } else {
+    memeTxt.innerText = '';
+    memeTxt.innerText = inputValue;
+  }
+}
+
+
+btnInsertTxt.addEventListener('click', addMemeText);
+
+function captureImg() {
+  const cptrImg = insertMeme.files;
+  // memeImg.
+}
+
+function fireBorder() {
+
+}
+
+function fwaterBorder() {
+
+}
+
+function earthBorder() {
+
+}
+
+btnFire.addEventListener('click', fireBorder);
