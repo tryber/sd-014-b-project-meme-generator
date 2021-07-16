6e0fa078660e2f0cf52af54cd353ffba56a4779a
Bruno Trindade <brunotrndd@gmail.com>
2021-07-16 14:36:24 -0300

    Ajustes e requisitos até o 4

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const input = document.getElementById('text-input');
-const memeContainer = document.querySelector('#meme-image-container');
+const meme = document.getElementById('meme-image');
+const imgInsert = document.getElementById('meme-insert');
 
-function createText() {
-  const textElement = document.createElement('p');
-  textElement.classList.add('meme-text');
-  memeContainer.appendChild(textElement);
-}
+// function createText() {
+//   const textElement = document.createElement('p');
+//   textElement.classList.add('meme-text');
+//   textContainer.appendChild(textElement);
+// }
 
-window.onload = createText;
+// window.onload = createText;
 
 function insertMemeText() {
-  const text = document.querySelector('#meme-image-container p');
+  const text = document.querySelector('#meme-text');
   if (input.textContent !== null) {
     text.innerHTML = input.value;
   }
 }
 
 input.addEventListener('input', insertMemeText);
+
+function renderImage() {
+  const imgInput = document.getElementById('meme-insert').files[0];
+  const reader = new FileReader();
+  if (imgInput) {
+    reader.readAsDataURL(imgInput);
+  }
+  reader.onloadend = function () {
+    meme.src = reader.result;
+  };
+}
+
+imgInsert.addEventListener('input', renderImage);
