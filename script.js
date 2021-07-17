b916e8d1e17130fec1a33bfc53976dbdbc475786
Victor Martins <victor.martins.duarte@gmail.com>
2021-07-17 20:14:32 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-let inputText = document.querySelector('#text-input');
-let memeText = document.querySelector('#meme-text')
+const inputText = document.querySelector('#text-input');
+const memeText = document.querySelector('#meme-text')
+const memeContainer = document.querySelector('#meme-image-container');
+const fireBtn = document.querySelector('#fire');
+const waterBtn = document.querySelector('#water');
+const earthBtn = document.querySelector('#earth');
 
 inputText.addEventListener('keyup', () => {
   memeText.innerText = inputText.value;
+});
+
+fireBtn.style.backgroundColor = 'red';
+fireBtn.addEventListener('click', () => {
+  memeContainer.style.border = '3px dashed red';
+});
+
+waterBtn.style.backgroundColor = 'blue';
+waterBtn.addEventListener('click', () => {
+  memeContainer.style.border = '5px double blue';
+});
+
+earthBtn.style.backgroundColor = 'green';
+earthBtn.addEventListener('click', () => {
+  memeContainer.style.border = '6px groove green';
 });
