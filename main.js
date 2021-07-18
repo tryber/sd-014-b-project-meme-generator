3b5fc5f69578e8e2b6d832cd26bf226c9e8e6db3
LuanaMB <luanabragaaa@hotmail.com>
2021-07-18 17:52:25 -0300

    ex 6

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const btnText = document.querySelector('#btn-text');
 const btnReset = document.querySelector('#btn-reset');
 const inputImg = document.querySelector('#meme-insert');
 const imagem = document.getElementById('meme-image');
+const memeBoard = document.querySelector('#meme-image-container');
+const btnFire= document.getElementById('fire');
+const btnWater= document.getElementById('water');
+const btnEarth= document.getElementById('earth');
 
 function inputText() {
   let input = textValue.value;
@@ -25,3 +29,13 @@ function image(event) {
 inputImg.addEventListener('change', image);
 btnText.addEventListener('click', inputText);
 btnReset.addEventListener('click', resetText);
+btnFire.addEventListener('click', function(){
+  memeBoard.style.border ='3px dashed red';
+})
+btnWater.addEventListener('click', function(){
+  memeBoard.style.border ='5px double blue';
+})
+btnEarth.addEventListener('click', function(){
+  memeBoard.style.border ='6px groove green'
+})
+
