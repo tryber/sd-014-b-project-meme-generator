f499125eed8b07a871c568546ff32f933e689c8b
LuanaMB <luanabragaaa@hotmail.com>
2021-07-18 17:20:37 -0300

    corrige erros

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const textValue = document.querySelector('#text-input');
 const h1Text = document.querySelector('#meme-text');
 const btnText = document.querySelector('#btn-text');
 const btnReset = document.querySelector('#btn-reset');
-const inputImg= document.querySelector('#meme-insert');
+const inputImg = document.querySelector('#meme-insert');
 const imagem = document.getElementById('meme-image');
 
 function inputText() {
@@ -16,13 +16,11 @@ function resetText() {
 function image(event) {
   const fileReader = event.target.files[0];
   const file = new FileReader();
-  file.onloadend = function(){
+  file.onloadend = function () {
     imagem.setAttribute('src', file.result);
-  }
+  };
   file.readAsDataURL(fileReader);
 }
-
-
 
 inputImg.addEventListener('change', image);
 btnText.addEventListener('click', inputText);
