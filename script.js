1883bbcc3bd0170f5db8c01bd3842ee7ec4aa0d5
Arturo Vaine <arturo.vaine@gmail.com>
2021-07-18 21:31:48 -0300

    Requisitos 1, 3(parcial), 5.

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
- 
+const textInput = document.getElementById('text-input');
+const textMeme = document.getElementById('meme-text');
+textInput.addEventListener('keyup', () => { textMeme.innerText = textInput.value; });
