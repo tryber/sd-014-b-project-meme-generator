4ca3a625e13d391b1f99171c3887e8270fd0fc5b
Arturo Vaine <arturo.vaine@gmail.com>
2021-07-19 09:26:59 -0300

    Alterações requisito 2.

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ const imageContainer = document.getElementById('meme-image-container');
 btnFire.addEventListener('click', () => { imageContainer.style.border = '3px dashed red'; });
 btnWater.addEventListener('click', () => { imageContainer.style.border = '5px double blue'; });
 btnEarth.addEventListener('click', () => { imageContainer.style.border = '6px groove green'; });
+
+const imageMeme = document.getElementById('meme-image');
+const imgInput = document.getElementById('meme-insert');
+const memes = document.querySelectorAll('.memes');
+
+if (imgInput.value) {
+  imageMeme.src = imgInput.value;
+}
+
+for (let i = 0; i < memes.length; i += 1) {
+  memes[i].addEventListener('click', (event) => {
+    imageMeme.src = event.target.src;
+  });
+}
+
+imgInput.onchange = evt => {
+  const [file] = imgInput.files;
+  if (file) {
+    imageMeme.src = URL.createObjectURL(file);
+  }
+};
