5d884ca25f4c8a29b7a6dcf15b012570dab86a08
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2021-07-20 10:45:51 -0300

    Projeto concluido

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 window.onload = function () {
 
-  let inputText = document.querySelector('#text-input');
-  let memeText = document.querySelector('#meme-text');
-  let inputFile = document.querySelector('#meme-insert');
-  let memeImg = document.querySelector('#meme-image');
-  let buttonFile = document.querySelector('#fileSelect');
+  const memeImgContainer = document.querySelector('#meme-image-container');
+  const inputText = document.querySelector('#text-input');
+  const memeText = document.querySelector('#meme-text');
+  const inputFile = document.querySelector('#meme-insert');
+  const memeImg = document.querySelector('#meme-image');
+  const buttonFile = document.querySelector('#fileSelect');
+  const buttonFire = document.querySelector('#fire');
+  const buttonWater = document.querySelector('#water');
+  const buttonEarth = document.querySelector('#earth');
+  const buttonsMemes = document.querySelectorAll('.box');
+
+  const buttonUpText = document.querySelector('#legUp');
+  const buttonDownText = document.querySelector('#legDown');
 
   inputText.addEventListener('keyup', function () {
     memeText.innerHTML = inputText.value;
@@ -12,12 +20,49 @@ window.onload = function () {
 
   inputFile.addEventListener('change', function () {
     let img = URL.createObjectURL(this.files[0]);
-    document.querySelector('#no-image').style.display = 'none';
-    memeImg.style.backgroundImage = 'url(' + img +')';
+    memeImg.src = img;
   });
 
   buttonFile.addEventListener('click', function () {
     inputFile.click();
+  });
+
+  /* Mudando a borda com os botões */
+
+  buttonFire.addEventListener('click', function () {
+    memeImgContainer.style.border = '3px dashed red';
+  });
+
+  buttonWater.addEventListener('click', function () {
+    memeImgContainer.style.border = '5px double blue';
+  });
+
+  buttonEarth.addEventListener('click', function () {
+    memeImgContainer.style.border = '6px groove green';
+  });
+
+  buttonsMemes[0].addEventListener('click', function () {
+    memeImg.src = 'imgs/meme1.png';
+  })
+
+  buttonsMemes[1].addEventListener('click', function () {
+    memeImg.src = 'imgs/meme2.png';
+  })
+
+  buttonsMemes[2].addEventListener('click', function () {
+    memeImg.src = 'imgs/meme3.png';
+  })
+
+  buttonsMemes[3].addEventListener('click', function () {
+    memeImg.src = 'imgs/meme4.png';
+  })
+
+  buttonDownText.addEventListener('click', function () {
+    memeText.style.alignSelf = 'flex-end';
+  })
+
+  buttonUpText.addEventListener('click', function () {
+    memeText.style.alignSelf = 'flex-start';
   })
 
 }
