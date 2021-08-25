03e57f1f250ac46a7e11cface36b9580e1705d53
LucasAccurcio <lucas.accurcio@gmail.com>
2021-08-25 15:13:58 -0300

    Requisito 04

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const inputText = document.getElementById('text-input');
-const textContainer = document.getElementById('meme-text');
+const memeText = document.getElementById('meme-text');
+const inputImg = document.getElementById('meme-image');
+const memeContainer = document.getElementById('meme-image-container')
 
 inputText.addEventListener('keyup', () => {
-  textContainer.innerText = inputText.value;
+  memeText.innerText = inputText.value;
+});
+
+inputImg.addEventListener('change', () => {
+  const img = document.createElement('img');
+  img.src = './img/20210604_110822.jpg';
+  img.style.width = '400px';
+  img.style.height = '400px';
+  memeContainer.appendChild(img);
+  console.log(inputImg.value);
 });
