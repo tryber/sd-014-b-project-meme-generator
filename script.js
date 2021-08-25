63ae63236b52a93f10bbf4f22fc60b9269f8aa1c
LucasAccurcio <lucas.accurcio@gmail.com>
2021-08-25 15:48:19 -0300

    Requisito 06

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const inputText = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 const inputImg = document.getElementById('meme-image');
-const memeContainer = document.getElementById('meme-image-container')
+const memeContainer = document.getElementById('meme-image-container');
+
+const btnFire = document.getElementById('fire');
+btnFire.addEventListener('click', () => {
+  memeContainer.style.border = '3px dashed rgb(255, 0, 0)';
+});
+
+const btnWater = document.getElementById('water');
+btnWater.addEventListener('click', () => {
+  memeContainer.style.border = '5px double rgb(0, 0, 255)';
+});
+
+const btnEarth = document.getElementById('earth');
+btnEarth.addEventListener('click', () => {
+  memeContainer.style.border = '6px groove rgb(0, 128, 0)';
+});
 
 inputText.addEventListener('keyup', () => {
   memeText.innerText = inputText.value;
